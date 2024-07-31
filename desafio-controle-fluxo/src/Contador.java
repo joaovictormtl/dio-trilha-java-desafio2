33c399c4947c750751e62f468e6d062502d4fffa
johnstack <joaovictormtl@gmail.com>
2024-07-31 19:29:02 -0300

    feat: adicionar funcionalidade de contagem no método

--- desafio-controle-fluxo/src/Contador.java
+++ desafio-controle-fluxo/src/Contador.java
@@ -8,6 +8,11 @@ public class Contador {
     static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
         if(parametroUm > parametroDois){
             throw new ParametrosInvalidosException();
-        } 
+        } else{
+            int contagem = parametroDois - parametroUm;
+            for(int i = 0; i < contagem; i++){
+                System.out.println("Imprimindo o número "+(i+1));
+            }
+        }
     }
 }
